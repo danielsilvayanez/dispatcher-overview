d4afc6d8d5fc05c8b9e5297404caf8022e07f57d
danielsilvayanez <daniel.silva@xing.com>
2020-09-25 16:39:29 +0200

    Update dispatcher form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Auftragsformular from './components/AuftragsFormular'
 
 function App() {
   return (
     <div>
       <h1>Dispatcher Overview</h1>
+      <Auftragsformular />
     </div>
   )
 }
